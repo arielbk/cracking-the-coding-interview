c824166c7886e5824372d02c5d09522ae5cb73d2
arielbk <buchwald.kearney@gmail.com>
2018-05-23 23:47:19 +0200

    bugfix

--- arrays-left-rotation.js
+++ arrays-left-rotation.js
@@ -1,20 +1,24 @@
 function solve(a, d) {
-
-    // Start with an empty array
     var arr = [];
-
-    // Populate the array
+    // Create the array
     for (let i = 1; i <= a; i++) {
         arr.push(i);
     }
 
-    // Rotate the array d times
+    // Rotate the array
     for (let i = 0; i < d; i++) {
 	    // remove the first element off the array and assign it to last
     	var last = arr.shift();
     	// push it to the end of the array
     	arr.push(last);
+	}
+
+    var answerString = '';
+
+    // Print the array as a string
+    for (let i = 0; i < arr.length; i++) {
+        answerString += i;
     }
 
-    return arr;
+    return answerString;
 }
