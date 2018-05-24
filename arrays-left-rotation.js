86209c6d288350be98506583eb6e62e62e9bed31
arielbk <buchwald.kearney@gmail.com>
2018-05-24 18:37:15 +0200

    fixed arrays left rotation

--- arrays-left-rotation.js
+++ arrays-left-rotation.js
@@ -1,24 +1,14 @@
-function solve(a, d) {
-    var arr = [];
-    // Create the array
-    for (let i = 1; i <= a; i++) {
-        arr.push(i);
-    }
+// Heh, I realised that much of the functionality is taken care of already
+// a is an array passed in, d is the number of times to rotate the array
+// returns a rotated array
 
+function solve(a, d) {
     // Rotate the array
     for (let i = 0; i < d; i++) {
 	    // remove the first element off the array and assign it to last
-    	var last = arr.shift();
+    	var last = a.shift();
     	// push it to the end of the array
-    	arr.push(last);
-	}
-
-    var answerString = '';
-
-    // Print the array as a string
-    for (let i = 0; i < arr.length; i++) {
-        answerString += i;
-    }
-
-    return answerString;
+    	a.push(last);
+	  }
+    return a;
 }
